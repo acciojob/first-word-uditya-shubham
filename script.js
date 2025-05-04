337a2aa95624a685f7bb9f9460188ba28b812ba5
Uditya Kumar <shubhamkumaar2001@gmail.com>
2025-05-04 15:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+str = str.trim();
+
+  const spaceIndex = str.indexOf(' ');
+
+  // If there's no space, return the whole string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Otherwise, return the substring up to the first space
+  return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
